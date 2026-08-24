c7ba9e27126a9f98b81304e5f65c2b98863a09cf
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-24 16:43:06 +0530

    tuples and list in details 1-data structure

---
+++ detailed knowledge on listss/list.py
@@ -0,0 +1,4 @@
+mylist=[1,2,3]
+mylist1=['STRING',2,3]
+print(mylist1[1])
+print(mylist1[0:])
---
+++ detailed knowledge on tuples/tuples.py
@@ -0,0 +1,16 @@
+t=(1,2,3)
+l=[1,2,3]
+print(type(t))
+print(type(l))
+print(len(t))
+print(len(l))
+t1=('one',1)
+print(t1[0])
+print(t1[-1])
+t3=('a','a','b')
+print("count repetition of element in tuple",t3.count('a'))
+print("index of a",t3.index('a'))
+mylist=[1,2,3]
+mylist[0]="NEW"
+print("list is mutable",mylist)
+t3[0]='NEW'
