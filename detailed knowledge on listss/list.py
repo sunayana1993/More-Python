71861c31f0d2bcad35d700073b04b4cd59df90b7
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-24 20:27:25 +0530

    list function

--- detailed knowledge on listss/list.py
+++ detailed knowledge on listss/list.py
@@ -2,3 +2,13 @@ mylist=[1,2,3]
 mylist1=['STRING',2,3]
 print(mylist1[1])
 print(mylist1[0:])
+
+#concatenate two list
+m1=['one','two','three']
+m2=['four','five','six']
+print(m1+m2)
+
+l2=[3,1,5,4]
+l2.sort()
+#print(l2.sort()) -prints None as sort is inplace
+print(l2)
