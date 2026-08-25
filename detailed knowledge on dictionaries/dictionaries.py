bc71649f32cdb769047bf49b134b40c59e37c51c
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-25 19:55:44 +0530

    data structure

---
+++ detailed knowledge on dictionaries/dictionaries.py
@@ -0,0 +1,7 @@
+dict={'key1':'value1','key2':'value2'}
+print(dict['key1'])
+price_lookup={'apple':12.59,'banana':6.60}
+print(price_lookup['banana'])
+print(price_lookup.values())
+print(price_lookup.keys())
+print(price_lookup.items())
--- detailed knowledge on listss/list.py
+++ detailed knowledge on listss/list.py
@@ -12,3 +12,7 @@ l2=[3,1,5,4]
 l2.sort()
 #print(l2.sort()) -prints None as sort is inplace
 print(l2)
+
+#How do I index a nested list?
+l3=[1,2,[4,5]]
+print(l3[2][0])
