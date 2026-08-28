48a7c742b08d6d81c38f4d9a09b7f647e28ae5ba
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-28 18:18:48 +0530

    add function

---
+++ methods/add_function.py
@@ -0,0 +1,4 @@
+def add_function(num1,num2):
+    return num1+num2
+
+print(add_function(1,2))
---
+++ methods/function.py
@@ -0,0 +1,5 @@
+def name_of_function():
+    #docstring explains function
+    print("Hello")
+
+name_of_function()
