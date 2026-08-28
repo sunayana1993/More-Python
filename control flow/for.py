a361211319ddc0bf293fea13b6dca7e8fbed4571
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-08-28 12:36:32 +0530

    function and method

--- control flow/for.py
+++ control flow/for.py
@@ -1,3 +1,18 @@
 mylist=[1,2,3,4,5,6,7,8,9]
 for item in mylist:
     print(item)
+
+mystring="Hello World"
+for item in mystring:
+    print(item)
+
+myitem=[(1,2),(2,3),(4,5)]
+for item in myitem:
+    print(item)
+
+for a,b in myitem:
+    print(b)
+
+d={'key1':'value1','key2':'value2'}
+for item in d:
+    print(item)
---
+++ methods/built_in.py
@@ -0,0 +1,5 @@
+mylist=[1,2,3]
+mylist.append(4)
+print(mylist)
+mylist.pop()
+print(mylist)
