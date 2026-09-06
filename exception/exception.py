2655896ac81eff39f4bb9669f45c5ae102706280
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-09-07 01:59:59 +0530

    exception

---
+++ exception/exception.py
@@ -0,0 +1,9 @@
+try:
+    result=10+'10'
+
+except:
+    print("you aren't adding up correctly")
+
+else:
+    print("Add")
+    print(result)
---
+++ exception/exception1.py
@@ -0,0 +1,12 @@
+try:
+    f=open('testfile','r')
+    f.write("Write a test line")
+
+except TypeError:
+    print("There was a type error")
+
+except OSError:
+    print("Hey you have an OS Error")
+
+finally:
+    print("I always run")
