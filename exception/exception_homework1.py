a2e50935e1555f713eaa2e53f8c4a0a5ee4f0f65
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-09-07 16:08:49 +0530

    zero division exception

---
+++ exception/exception_homework1.py
@@ -0,0 +1,5 @@
+try:
+    for i in ['a','b','c']:
+        print(i*i)
+except TypeError:
+    print("this is type error")
---
+++ exception/exception_homework2.py
@@ -0,0 +1,6 @@
+try:
+    x=5
+    y=0
+    print(x/y)
+except ZeroDivisionError:
+    print(" zero division error")
