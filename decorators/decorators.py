054c2e978b63f7c8de032e41dd8fe5da5039ca76
sunayana1993 <sunayanabhowmik1993@gmail.com>
2026-09-10 19:08:54 +0530

    decorator

---
+++ decorators/decorators.py
@@ -0,0 +1,12 @@
+#add extra functionality easily with decorator @
+#@some_decorator 
+#def_simple func():
+#Do simple stuff, return something
+def func():
+    return 1
+print(func())
+
+def hello():
+    return "Hello"
+
+print(hello())
